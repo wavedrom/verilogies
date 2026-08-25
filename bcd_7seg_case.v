3b8fb6e4d7fb7f10f714c9ec6322767ab955c5e8
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2026-08-25 14:58:25 -0700

    fixed bcd 7seg truth table

--- bcd_7seg_case.v
+++ bcd_7seg_case.v
@@ -13,23 +13,23 @@ always @*
   //  e     c
   //   ==d==
   case (bcd)
-    4'h0: seg_n = 7'b100_0000; // 0
-    4'h1: seg_n = 7'b111_1001; // 1
-    4'h2: seg_n = 7'b010_0100; // 2
-    4'h3: seg_n = 7'b011_0000; // 3
-    4'h4: seg_n = 7'b001_1001; // 4
-    4'h5: seg_n = 7'b001_0010; // 5
-    4'h6: seg_n = 7'b000_0000; // 6 (includes top segment 'a' per 7447 spec)
-    4'h7: seg_n = 7'b111_0000; // 7
-    4'h8: seg_n = 7'b000_0000; // 8
-    4'h9: seg_n = 7'b001_0000; // 9
+    4'h0: seg_n = 7'b1000000; // 0
+    4'h1: seg_n = 7'b1111001; // 1
+    4'h2: seg_n = 7'b0100100; // 2
+    4'h3: seg_n = 7'b0110000; // 3
+    4'h4: seg_n = 7'b0011001; // 4
+    4'h5: seg_n = 7'b0010010; // 5
+    4'h6: seg_n = 7'b0000011; // 6 -- missing top bar 'a'
+    4'h7: seg_n = 7'b1111000; // 7
+    4'h8: seg_n = 7'b0000000; // 8
+    4'h9: seg_n = 7'b0011000; // 9 -- missing bottom bar 'd'
     // Standard 7447 non-digit symbols for values 10 to 15:
-    4'hA: seg_n = 7'b000_1000; // [
-    4'hB: seg_n = 7'b000_0011; // ]
-    4'hC: seg_n = 7'b001_1100; // u
-    4'hD: seg_n = 7'b010_0001; //
-    4'hE: seg_n = 7'b000_0110; //
-    4'hF: seg_n = 7'b111_1111; // Blank
+    4'hA: seg_n = 7'b0100111; // ⊏ -- Mutilated C, Inverted 'c', Pseudo-Hex 'A'
+    4'hB: seg_n = 7'b1100111; // ⊐ -- Mutilated J, Backward 'J' / Pseudo-Hex 'b'
+    4'hC: seg_n = 7'b1011100; // ⊓ -- Mutilated H / Upper Hook / "nH" shape / Pseudo-Hex 'C'
+    4'hD: seg_n = 7'b0010110; // ⊔ -- Mutilated U / Symmetric Open Cup / Pseudo-Hex 'd'
+    4'hE: seg_n = 7'b0000111; // ⊦ -- Mutilated E / Cross Bar / T-hook / Pseudo-Hex 'E'
+    4'hF: seg_n = 7'b1111111; // Blanking State / Overflow Output / Pseudo-Hex 'F'
     default: seg_n = 7'b111_1111;
 endcase
 
